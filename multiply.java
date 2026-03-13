620e43b09d4d865661e541457c51e01e2969fd4a
Harshada471 <harshadadeshmukh888@gmail.com>
2026-03-13 07:12:14 +0530

    Updated multiply file in feature branch

--- multiply.java
+++ multiply.java
@@ -1,8 +1,8 @@
 public class Multiply { 
     public static void main(String[] args){
         
-        int a = 5; 
-        int b = 4; 
+        int a = 20; 
+        int b = 10; 
         
         System.out.println("Multiplication is " + (a * b)); 
 } 
